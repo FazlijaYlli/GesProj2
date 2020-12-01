15b342bc81e64f62a35319481cd547a09dc713ed
Hugo <hugo.ducommun@bluewin.ch>
2020-12-01 15:56:34 +0100

    Updated database and first datatable in HTMl file

--- temp/db_gesproj2_grp2.sql
+++ temp/db_gesproj2_grp2.sql
@@ -3,7 +3,7 @@
 -- *--------------------------------------------
 -- * DB-MAIN version: 11.0.1              
 -- * Generator date: Dec  4 2018              
--- * Generation date: Tue Dec  1 14:40:52 2020 
+-- * Generation date: Tue Dec  1 15:37:15 2020 
 -- * Schema: db_gesproj2_grp2/mld 
 -- ********************************************* 
 
@@ -24,15 +24,16 @@ create table t_imprimante (
      impMarque varchar(50) not null,
      impModele varchar(100) not null,
      impTechImpression varchar(50) not null,
-     impVitesseImpression int not null,
+     impVitesseImpression float(1) not null,
      impCapaciteBac int not null,
-     impPoids int not null,
+     impPoids float(1) not null,
      impResolutionX int not null,
      impResolutionY int not null,
-     impLongueur int not null,
-     impLargeur int not null,
-     impHauteur int not null,
-     impPrix int not null,
+     impLongueur float(1) not null,
+     impLargeur float(1) not null,
+     impHauteur float(1) not null,
+     impPrix float(1) not null,
+     impImage varchar(200) not null,
      constraint ID_t_imprimante_ID primary key (idImprimante));
 
 
